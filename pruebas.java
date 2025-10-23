4b9c1d266efadea407ed6209a1e4c3e9af17819e
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2025-10-23 14:10:55 +0200

    V0.1

---
+++ pruebas.java
@@ -0,0 +1,37 @@
+public class pruebas {
+
+    public static void array(int[] args){
+        int[]vector1={3,4,6};
+        for(int i=0; i<vector1.length; i++){
+            System.out.print(vector1[i] + " ");
+        }
+        
+        int[]vector2={1,2,3};
+        int b=0;
+        while (b<vector2.length) {
+            System.out.print(vector2[b] + " ");
+            b++;
+        }
+        
+        int[]array={7,9,8};
+        int a = 0;
+        do {
+            System.out.print(array[a] + " ");
+            a++;           
+        } while (a < 3);
+    }
+    public static void main(String[] args) {
+        
+    class Innerpruebas {}
+    int c = 0;
+        do {
+            System.out.println("que pase el numero: " + c);
+            c ++;
+        } while (c < 9);
+            if (c == 7) {
+                
+            }
+        
+    
+    }
+}
